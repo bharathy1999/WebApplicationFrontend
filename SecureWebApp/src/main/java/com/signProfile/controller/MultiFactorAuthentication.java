ac90baa74cabf2e6c6a828687e4ed958c399e998
bharathy1999 <121527343+bharathy1999@users.noreply.github.com>
2024-05-03 11:38:29 +0530

    Update MultiFactorAuthentication.java

--- SecureWebApp/src/main/java/com/signProfile/controller/MultiFactorAuthentication.java
+++ SecureWebApp/src/main/java/com/signProfile/controller/MultiFactorAuthentication.java
@@ -1,57 +1,106 @@
 package com.signProfile.controller;
 
-import java.util.Properties;
-import java.util.Random;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// This class provides to sent a otp to sigin user email for verification
-public class MultiFactorAuthentication {
+import com.signProfile.DAO.GetProfile;
+import com.signProfile.model.EncryptModel;
 
-	public String sendOTP(String toEmail) {
-		// this Random object give a random number from 0 to 999999 for otp
-		Random random = new Random();
-		int randomNumber = random.nextInt(999999);
+public class SignIn extends HttpServlet {
 
-		String senderEmail = "guruvijay2017@gmail.com";
-		String password = "nibx xaao aygw dbuh";
-
-		Properties properties = new Properties();
-		properties.put("mail.smtp.host", "smtp.gmail.com");// used a SMTP server for sending a mail to user
-		properties.put("mail.smtp.auth", "true");// authentication is used when connecting to a SMTP server
-		properties.put("mail.smtp.port", "587");// this port is for TLS based SMTP server connection
-		properties.put("mail.smtp.starttls.enable", "true");// this enable TLS connection
-		properties.put("mail.smtp.ssl.protocols", "TLSv1.2");// it specifies TLS version
-
-		Session session = Session.getInstance(properties, new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(senderEmail, password);
-			}
-		});
-
-		// this is responsible for send a message to user
+	private String mfaOtp;
+// this method is used for decrypt email
+	public static String decrypt(String encryptedText, SecretKey secretKey) {
+		String email = null;
 		try {
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(senderEmail));
-			message.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
-			message.setSubject("Your Trainee verification from ****");
-			// In case the random object gives less than 6 numbers it add a zeros for
-			// additional digits
-			message.setText("your otp number is" + String.format("%06d", randomNumber));
-			Transport.send(message);
-		} catch (MessagingException e) {
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+			byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
+			byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+			email = new String(decryptedBytes);
+		} 
+		// In case there is no support for given algorithm and gives a invalid key for decryption
+		catch (NoSuchAlgorithmException | InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		System.out.println(randomNumber);
-
-		return String.format("%06d", randomNumber);
+		return email;
 	}
+// this method gives secret key
+	
+	public static SecretKey stringToKey(String keyString) {
+		byte[] decodedKey = Base64.getDecoder().decode(keyString);
+		return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+	}
+
+	public static String hashing(String input) {
+		StringBuilder hexString = new StringBuilder();
+
+		try {
+			MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+			byte[] hashBytes = sha256.digest(input.getBytes());
+			for (byte hashByte : hashBytes) {
+				String hex = Integer.toHexString(0xff & hashByte);
+				hexString.append(hex);
+			}
+			// In case there is no support for given algorithm
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return hexString.toString();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		String otp = request.getParameter("otp");
+		PrintWriter out = response.getWriter();
+// when a user initially log in this block gets executed after this otp is sent to user 
+		if (otp == null) {
+			GetProfile getProfile = new GetProfile();
+			try {
+				EncryptModel encryptModel = getProfile.retrieveProfile(name, hashing(password));
+				System.out.println(encryptModel);
+				String mail = decrypt(encryptModel.getMail(), stringToKey(encryptModel.getKey()));
+				System.out.println();
+				MultiFactorAuthentication multiFactorAuthentication = new MultiFactorAuthentication();
+				mfaOtp = multiFactorAuthentication.sendOTP(mail);
+				out.write("SUCCESS");
+			} catch (Exception ne) {
+				out.write("FAILURE");
+			}
+
+		}
+// otp is not null this block gets executed
+		else {
+			System.out.println(mfaOtp);
+			System.out.println(otp);
+			if (otp.equals(mfaOtp)) {
+				out.write("SUCCESS");
+			} else {
+				out.write("FAILURE");
+			}
+
+		}
+
+	}
+
 }
