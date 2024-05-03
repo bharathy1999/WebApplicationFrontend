f0195c0fdb90abf9172999b8153e2ceda3f9ac39
bharathy1999 <121527343+bharathy1999@users.noreply.github.com>
2024-05-03 11:40:46 +0530

    Update EncryptModel.java

--- SecureWebApp/src/main/java/com/signProfile/model/EncryptModel.java
+++ SecureWebApp/src/main/java/com/signProfile/model/EncryptModel.java
@@ -1,21 +1,20 @@
 package com.signProfile.model;
-
+// this is pojo class for getting encrypted email and the coressponding key
 public class EncryptModel {
-  private String email;
-  private String key;
-  public EncryptModel(String email,String key){
-	   this.email=email;
-	   this.key=key;
-   }
-  
-  public String getMail() {
-	  return this.email;
-  }
-  
-  public String getKey() {
-	  return this.key;
-  }
-  
-  
-  
+	private String email;
+	private String key;
+
+	public EncryptModel(String email, String key) {
+		this.email = email;
+		this.key = key;
+	}
+
+	public String getMail() {
+		return this.email;
+	}
+
+	public String getKey() {
+		return this.key;
+	}
+
 }
